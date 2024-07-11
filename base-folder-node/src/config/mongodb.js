e81598a13c85d600e708426589caf0436e524c6b
Nguyễn Thế An <nguyenthean12062002@gmail.com>
2024-07-11 19:13:43 +0700

    update base folder node

---
+++ base-folder-node/src/config/mongodb.js
@@ -0,0 +1,7 @@
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+const MONGODB_URL =
+  "mongodb+srv://nguyenthean12062002:<password>@booking-tour.ybc3772.mongodb.net/?retryWrites=true&w=majority&appName=booking-tour";
